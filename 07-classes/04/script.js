da27489cf77f7c4a04c8e0c784d01d0939d10cc4
AyoubLegouirah <ayoubgrand@outlook.fr>
2023-10-26 14:19:40 +0200

    js

--- 07-classes/04/script.js
+++ 07-classes/04/script.js
@@ -9,6 +9,32 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(() => {
-    // your code here
-})();
+// Classe Person avec les propriétés firstname et lastname
+class Person {
+    constructor(firstname, lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    get name() {
+        return `${this.firstname} ${this.lastname}`;
+    }
+
+    set name(fullName) {
+        const parts = fullName.split(' ');
+        this.firstname = parts[0];
+        this.lastname = parts[1];
+    }
+}
+
+document.getElementById("run").addEventListener("click", () => {
+    // Crée une instance de la classe Person
+    const person = new Person("John", "Doe");
+
+    // Affiche la valeur de name dans la console
+    console.log("Nom complet initial : " + person.name);
+
+    // Affecte une nouvelle valeur à name et affiche l'état de l'instance
+    person.name = "Alice Smith";
+    console.log("Nouveau nom complet : " + person.name);
+});
